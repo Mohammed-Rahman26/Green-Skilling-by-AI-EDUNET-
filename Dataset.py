383570d02bd15524ecde93e1f9068803ab9ea778
Mohammed Rahman <26rahman2004@gmail.com>
2025-07-23 21:24:29 +0530

    Dataset.py

---
+++ Dataset.py
@@ -0,0 +1,7 @@
+climate_data = [
+    {"city": "City A", "temperature": 25, "carbon_footprint": 500},
+    {"city": "City B", "temperature": 30, "carbon_footprint": 350},
+    {"city": "City C", "temperature": 22, "carbon_footprint": 600},
+    {"city": "City D", "temperature": 15, "carbon_footprint": 200},
+    {"city": "City E", "temperature": 28, "carbon_footprint": 450},
+]
