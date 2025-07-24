943cc18535023ab8466840ec4cf3a59f45c106a8
Mohammed Rahman <26rahman2004@gmail.com>
2025-07-24 12:13:01 +0530

    Problem11.py

---
+++ Problem11.py
@@ -0,0 +1,9 @@
+#11 Import NumPy and create a datset.
+import numpy as np
+
+# Energy consumption in MWh for different renewable sources: Solar, Wind, Hydropower, Geothermal, Biomass
+energy_consumption = np.array([1200, 3400, 2900, 1800, 2500])
+
+# Print the array
+print("Energy Consumption (in MWh) for Different Renewable Sources:")
+print(energy_consumption)
